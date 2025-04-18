8d9d49adfe148e0139c1b07fd7f453a09f30c5f0
kusuma obbina <o.kusuma1998@gmail.com>
2025-04-18 15:44:15 +0100

    second index

--- index.py
+++ index.py
@@ -1,16 +1,16 @@
 import unittest
 from unittest.mock import patch, Mock
 from weather import get_data
+from conftest import sample_data
 
-class TestGetData(unittest.TestCase):
-    @patch('requests.get') 
-    def test_get_data(self, mock_get):
 
-        mock_response = Mock()
-        mock_response.status_code = 200 
-        mock_get.return_value = mock_response
-        result = get_data("Ireland")
-        self.assertEqual(result, 200)
-  
-if __name__ == '__main__': 
-    unittest.weather(argv=['first-arg-is-ignored'], exit=False) 
+@patch('requests.get') 
+def test_get_data(mock_get, sample_data):
+
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_get.return_value = mock_response 
+
+    city = sample_data["city"]
+    result = get_data(city)
+    assert result == 200
